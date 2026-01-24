92443acd81e6c5f811bbab14190f6705f84cba3a
OrZenaty <or.zenaty201430@gmail.com>
2026-01-25 00:35:27 +0200

    Updated log when initialized

--- app.js
+++ app.js
@@ -36,4 +36,6 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT} updated...`),
+);
